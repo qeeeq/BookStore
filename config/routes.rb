4026a1064c764fa94046e63aa382c78d8452b743
qeeeeeq <qeeeeeq@gmail.com>
2020-07-28 21:36:17 +0300

    design_index_v04

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+	root "books#index"
+	get 'index', to: 'books#index'
   devise_for :customers
   ActiveAdmin.routes(self)
-	root "books#index"
   resources :books
   resources :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

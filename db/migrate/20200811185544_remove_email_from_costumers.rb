04377a4d521a41bcef57e9cdc5b19edc43c81acc
qeeeeeq <qeeeeeq@gmail.com>
2020-08-12 02:16:10 +0300

    add books v2

--- db/migrate/20200811185544_remove_email_from_costumers.rb
+++
@@ -1,8 +0,0 @@
-class RemoveEmailFromCostumers < ActiveRecord::Migration[6.0]
-  def change
-  	remove_column :customers, :email, :string
-  	remove_column :customers, :encrypted_password, :string
-  	remove_column :customers, :reset_password_token, :string
-
-  end
-end

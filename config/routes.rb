bf5b8101c46f972fefddcd0e5c77426d0d44a28a
qeeeeeq <qeeeeeq@gmail.com>
2020-08-28 05:09:10 +0300

    add partial_form view for order

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,15 @@ Rails.application.routes.draw do
   root "books#index"
   resources :books
   resources :orders
+  # get orders/:status, to "orders#show", as: orders 
+  
   resources :credit_cards
   # post '/add_to_order/:order_item_id', to: 'orders#add_to_order', as: 'add_to_order'
   resources :order_items #only: %i[create destroy]
+
 
   ActiveAdmin.routes(self)
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
-
-
-
 
